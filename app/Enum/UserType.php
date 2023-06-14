f3c5bfdcdae9163f3e62a488467cb64bfc669842
João Victor Silva Soares <jovictors.soares@gmail.com>
2023-06-14 14:22:31 -0400

    feat: create User and UserType model + migrations

---
+++ app/Enum/UserType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enum;
+
+class UserType
+{
+    public const ID_SUPER_ADMIN = 1;
+    public const ID_ADMIN = 2;
+    public const ID_MEMBER = 3;
+
+    public const LABEL_SUPER_ADMIN = 'Super Admin';
+    public const LABEL_ADMIN = 'Admin';
+    public const LABEL_MEMBER = 'Member';
+}
--- database/migrations/2023_06_07_011944_create_users_table.php
+++ database/migrations/2023_06_07_011944_create_users_table.php
@@ -17,10 +17,15 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->date('birth_date');
+            $table->string('address', 200);
+            $table->string('city', 100);
+            $table->string('country', 100);
+            $table->unsignedBigInteger('user_type_id');
             $table->timestamps();
+            $table->foreign('user_type_id')->references('id')->on('user_types');
         });
     }
 
