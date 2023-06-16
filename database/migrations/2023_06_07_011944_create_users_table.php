d58b1b9f1b0f4a0acc2b9bb3a62f9c3060e07a12
João Victor Silva Soares <jovictors.soares@gmail.com>
2023-06-16 19:22:23 -0400

    feat: add nullable columns at user table

--- database/migrations/2023_06_07_011944_create_users_table.php
+++ database/migrations/2023_06_07_011944_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enum\UserType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,11 +20,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
-            $table->date('birth_date');
-            $table->string('address', 200);
-            $table->string('city', 100);
-            $table->string('country', 100);
-            $table->unsignedBigInteger('user_type_id');
+            $table->date('birth_date')->nullable();
+            $table->string('address', 200)->nullable();
+            $table->string('city', 100)->nullable();
+            $table->string('country', 100)->nullable();
+            $table->unsignedBigInteger('user_type_id')->default(UserType::ID_MEMBER);
             $table->timestamps();
             $table->foreign('user_type_id')->references('id')->on('user_types');
         });
